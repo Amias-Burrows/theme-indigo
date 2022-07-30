4209c6f5ee9244777c1897f4fdf239a52a216a03
amias-burrows <amias.burrows@gmail.com>
2022-07-30 10:13:50 +0100

    Added Theme info and started including other files

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
+<?php get_header(); ?>
 		<main>
 			<div class='newsletter'>
 				<h2>Join Our Tribe</h2>
 				<p>Lorem ipsum dolor sit amet, adipiscing.  The cow jumped over the moon.  He then stole my keys and shoes.  He took my life, impersonated my wife; and now lives with uncle Gru.</p>
+				<?php get_search_form(); ?>
 				<form class='contact'>
 					<label for='name'>Name</label>
 					<input name='name' placeholder='Your biggest fan'/>
@@ -11,3 +13,4 @@
 				</form>
 			</div>
 		</main>
+<?php get_footer(); ?>
